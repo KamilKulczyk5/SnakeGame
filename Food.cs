4e08d8613a1ff52de710368aeabe6237b3094891
KamilKulczyk5 <kulczykk5@gmail.com>
2025-05-28 00:44:22 +0200

    Dodano klasę Food do generowania jedzenia

---
+++ Food.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+class Food
+{
+    public (int x, int y) Position { get; private set; }
+
+    public Food()
+    {
+        GenerateNew();
+    }
+
+    public void GenerateNew()
+    {
+        Random rand = new Random();
+        Position = (rand.Next(0, Console.WindowWidth), rand.Next(0, Console.WindowHeight));
+        Console.SetCursorPosition(Position.x, Position.y);
+        Console.Write("*");
+    }
+}
